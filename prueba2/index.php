370353433e698d395aad52dc949e3fbb9a374286
Jesus E Cesin F <85196427+jesuscesin@users.noreply.github.com>
2022-08-01 11:15:02 -0400

    Add files via upload

---
+++ prueba2/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turbodal</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/main.js"></script>
+    <link rel="shortcut icon" href="favicon.png">
+
+
+
+</head>
+<body>
+    <div class="slideshow">
+
+        <ul class="slider">
+            <li>
+                <img src="img/1.jpg" alt="">
+                <section class="caption">
+                    <h1 id="primertexto"></h1>
+                </section>
+            </li>
+            <li>
+                <img src="img/2.jpg" alt="">
+                <section class="caption">
+                    <h1 id="">ESPECIALISTAS<BR>EN TURBOS</h1>
+                    <h4>NUEVOS Y REPARACION</h4>
+                </section>
+            </li>
+            <li>
+                <img src="img/3.jpg" alt="">
+                <section class="caption">
+                    <h1>COBERTURA NACIONAL<BR></h1>
+                    <h4>Tenemos la mejor cobertura nacional, revisa nuestras SUCURSALES</h4>
+                </section>
+            </li>
+        </ul>
+        <!--<ol class="pagination">--></ol>
+
+       
+
+ 
+    </div>
+    <div class ="flechas">
+            <ul>
+                <li>
+		            <a class="left">
+			        <span class="fa fa-chevron-left"></span>
+                    <a class="right">
+                    <span class="fa fa-chevron-right"></span></a>
+                    </a>
+                </li>
+            </ul>
+    </div>
+ 
+</body> 
+</html>
